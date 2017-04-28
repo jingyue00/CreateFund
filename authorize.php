e0cc13ac2e89762d83c6bc9e781b822f915bd0e2
Jerry <chaoqunhuang@172-16-44-28.DYNAPOOL.NYU.EDU>
2017-04-28 16:19:11 -0400

    Add captcha

--- authorize.php
+++ authorize.php
@@ -9,6 +9,11 @@
 	session_start();
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
+    $code = $_POST["code"];
+    if($code != $_SESSION["code"])
+    {
+         echo "<script>window.location.href='login.php'; alert('Incorrect Captcha. Please re-enter!');</script>";
+    }
     $loginname = $_POST["loginname"];
 	$password = $_POST["password"];
 	$md5password = md5($password);
