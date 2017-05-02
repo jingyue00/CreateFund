65e57f5dadeb2702b899c08d2f673e90aa73f18c
Jiwei Yu <yojy2268xnyu.edu>
2017-05-02 16:01:33 -0400

    try
    try

--- app.js
+++
@@ -1,8 +0,0 @@
-var trs = document.querySelectorAll(".glyphicon");
-var i;
-for(i=0;i<trs.length;i++)
-{
-    trs[i].addEventListener("click",function(e){
-        alert("you have selected : "+ e.target.id);
-    });
-}
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	unset($loginanme);
-	header("Location:index.php");
-    exit;
-?>
